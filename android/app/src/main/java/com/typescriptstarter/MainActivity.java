d6cb4b9bc1802db8e8985041af894caa7078fe19
Jesse Cooper <jesse@iterate.co>
2021-04-06 15:47:23 -0700

    react-native-gesture-handler android support

--- android/app/src/main/java/com/typescriptstarter/MainActivity.java
+++ android/app/src/main/java/com/typescriptstarter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.typescriptstarter;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,21 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TypescriptStarter";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 }
