4af88071e1376ee4e9dfcd5d0f5c2f2ebac61e00
Jesse Cooper <jesse@iterate.co>
2021-07-26 14:28:27 -0700

    upgraded to rn 0.64

--- android/app/src/main/java/com/typescriptstarter/MainActivity.java
+++ android/app/src/main/java/com/typescriptstarter/MainActivity.java
@@ -1,11 +1,6 @@
 package com.typescriptstarter;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,21 +11,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TypescriptStarter";
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
   }
 }
